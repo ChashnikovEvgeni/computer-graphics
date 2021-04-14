f5ea67ebef30e8a054b589c03df3c5d22e1eed87
godhealsofficial <rafael.kulikov@gmail.com>
2021-04-14 21:43:18 +0300

    rework with comments

--- RT/lab_1_rt/lab_1_rt/Material.cpp
+++ RT/lab_1_rt/lab_1_rt/Material.cpp
@@ -31,8 +31,31 @@ bool IdealMirror::Scatter(const Ray& ray_in, const SurfHit& surf, float3& attenu
     return (dot(ray_out.d, surf.normal) > 0);
 }
 
+
 bool Diffuse::Scatter(const Ray& ray_in, const SurfHit& surf, float3& attenuation, Ray& ray_out)
 {
+ /*   float randX = abs(float(rand()) / RAND_MAX);
+    float randY = abs(float(rand()) / RAND_MAX);
+
+    float3 tang, btang;
+    tang.x = 0.0f;
+    tang.y = -1.0f;
+    tang.z = surf.normal.y / surf.normal.x;
+    tang = normalize(tang);
+
+    btang = normalize((surf.normal, tang));
+
+    float3 diff = tang * (sqrt(randX)) * cos(2 * PI * randY) + btang * (sqrt(randX)) * sin(2 * PI * randY) + surf.normal * sqrt(1 - randX);
+    diff = normalize(diff);
+
+    ray_out = Ray(surf.hitPoint + surf.normal * 1e-5, diff);
+
+    attenuation = color * dot(ray_out.d, surf.normal);
+
+    return true;
+
+    btang = normalize(cross(tang, surf.normal));*/
+
     float kd = dot(normalize(ray_out.d), normalize(surf.normal));
 
 
